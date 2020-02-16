733c75dfda9f774e33a11b21f61220b3391b9982
Winston Pemberton <winstonpemberton@gmail.com>
2020-02-16 18:27:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -49,6 +49,13 @@ class Dog
     dog
   end
 
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    new_student = self.new(name, grade, id)
+    new_student
+  end
 
 
 end
